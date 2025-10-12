98b5de3f0d5e0e1cee1a74eb5e492bf6dd95ec9f
hddgpp <youssefhehe3@gmail.com>
2025-10-12 22:24:43 +0100

    creating the random number btn logic

--- Tenzies/src/components/Main/Main.jsx
+++ Tenzies/src/components/Main/Main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import './Main.css'
 
 export default function Main() {
-    return(
-        <main>
-            
-        </main>
-    )
+  const [nums, setNums] = React.useState(
+    Array(10).fill(1) // start all at 1
+  )
+
+  function randomNum(index) {
+    setNums(prev => {
+      const newNums = [...prev]
+      newNums[index] = Math.floor(Math.random() * 6) + 1
+      return newNums
+    })
+  }
+
+  return (
+    <main>
+      <div className='number-c'>
+        {nums.map((num, i) => (
+          <button key={i} className='btn' onClick={() => randomNum(i)}>
+            {num}
+          </button>
+        ))}
+      </div>
+    </main>
+  )
 }
