d2346f6387334862611238b2349e068207d9d79d
hddgpp <youssefhehe3@gmail.com>
2025-10-23 18:21:56 +0100

    setting the btn logic in a diceElements const

--- Tenzies/src/components/Main/Main.jsx
+++ Tenzies/src/components/Main/Main.jsx
@@ -9,19 +9,21 @@ export default function Main() {
   function randomNum(index) {
     setNums(prev => {
       const newNums = [...prev]
-      newNums[index] = Math.floor(Math.random() * 6) + 1
+      newNums[index] = Math.ceil(Math.random() * 6)
       return newNums
     })
   }
 
-  return (
-    <main>
-      <div className='num-c'>
-        {nums.map((num, i) => (
+  const diceElements = nums.map((num, i) => (
           <button key={i} className='btn' onClick={() => randomNum(i)}>
             {num}
           </button>
-        ))}
+        ))
+
+  return (
+    <main>
+      <div className='num-c'>
+        {diceElements}
       </div>
     </main>
   )
