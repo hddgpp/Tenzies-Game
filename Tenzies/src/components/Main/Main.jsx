cd361a4fef02bbb8983468a380352ce5de5b4492
hddgpp <youssefhehe3@gmail.com>
2025-10-24 19:25:54 +0100

    adding roll btn logic

--- Tenzies/src/components/Main/Main.jsx
+++ Tenzies/src/components/Main/Main.jsx
@@ -2,29 +2,26 @@ import React from 'react'
 import './Main.css'
 
 export default function Main() {
-  const [nums, setNums] = React.useState(
-    Array(10).fill(1)
-  )
+  const [nums, setNums] = React.useState(Array(10).fill(1))
 
-  function randomNum(index) {
-    setNums(prev => {
-      const newNums = [...prev]
-      newNums[index] = Math.ceil(Math.random() * 6)
-      return newNums
-    })
+  // this function rolls all dice at once
+  function rollAll() {
+    setNums(prev => prev.map(() => Math.ceil(Math.random() * 6)))
   }
 
+  // each button now just shows the number, no click handler
   const diceElements = nums.map((num, i) => (
-          <button key={i} className='btn' onClick={() => randomNum(i)}>
-            {num}
-          </button>
-        ))
+    <button key={i} className='btn'>
+      {num}
+    </button>
+  ))
 
   return (
     <main>
       <div className='num-c'>
         {diceElements}
       </div>
+      <button className='roll-btn' onClick={rollAll}>Roll</button>
     </main>
   )
 }
